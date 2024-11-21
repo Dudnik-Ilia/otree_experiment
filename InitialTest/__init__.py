90936385185a6d8df67cecaec73f103e34294f15
Dudnik-Ilia <dudnik.ilja@rambler.ru>
2024-11-21 13:12:57 +0100

    adjusted explanations

--- InitialTest/__init__.py
+++ InitialTest/__init__.py
@@ -71,23 +71,72 @@ class Intro(Page):
     form_model = 'player'
 
 
+EXAMPLE_SAMPLE = {
+	"Personal status and sex": "Male: single",
+	"Housing status": "Owned house",
+	"Credit history": "Established but risky credit history",
+	"Bank account status": "No account in bank",
+	"Savings account status": "More than 5k $",
+	"Other credits": "No other credits",
+	"Age (in years)": 40,
+	"Installment rate: % of income used for loan payments": 1,
+	"Credit amount (applicant has requested)": 10000,
+	"Loan duration (months)": 6
+}
+
 class ExampleQuestion(Page):
     form_model = 'player'
     form_fields = ['example_question']
     timeout_seconds = 120
     @staticmethod
-    def before_next_page(player: Player, timeout_happened):
-        # Make correct choice for future explanation
-        player.example_question="Approve"
+    def vars_for_template(player):
+        return {
+            'title': "Example Question",
+            'sample_dict': EXAMPLE_SAMPLE,
+        }
 
 
 class ExampleExplanation(Page):
     form_model = 'player'
-    form_fields = ['example_question']
-
+    timeout_seconds = 120
+    @staticmethod
+    def vars_for_template(player):
+        return {
+            'title': "Example Explanation",
+            'sample_dict': EXAMPLE_SAMPLE,
+        }
 
 class TestStart(Page):
     form_model = 'player'
+
+
+# Quiz Page for all quizes
+class Question(Page):
+    form_model = 'player'
+    timeout_seconds = 60
+    @property
+    def form_fields(self):
+        # Use player's current question to set form_fields dynamically
+        return [f"Question{self.player.current_question}"]
+    @staticmethod
+    def vars_for_template(player):
+        # Get the current quiz field from the generator
+        question_num = player.current_question
+        return {
+            'title': f"Question {question_num}/{NUMBER_OF_QUESTIONS_INITIAL_TEST}",
+            'sample_dict': SAMPLES_INITIAL_TEST.iloc[question_num-1].to_dict()
+        }
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Access the current question
+        question_num = player.current_question
+        user_answer = getattr(player, 'Question'+str(question_num))
+        real_answer = SAMPLES_INITIAL_TEST['class'].iloc[question_num-1]
+        if (user_answer == "Approve" and real_answer == 1) or (user_answer == "Decline" and real_answer == 0):
+            player.participant.payoff += player.random_number()
+            player.correct_answers += 1
+        # Increase question number
+        player.current_question += 1
 
 
 class ConfirmActive(Page):
@@ -101,39 +150,6 @@ class ConfirmActive(Page):
             player.participant.vars["dropout"]=True
             return 'Dropout'
 
-
-# Quiz Page for all quizes
-class Question(Page):
-    form_model = 'player'
-    timeout_seconds = 60
-    
-    @property
-    def form_fields(self):
-        # Use player's current question to set form_fields dynamically
-        return [f"Question{self.player.current_question}"]
-
-    @staticmethod
-    def vars_for_template(player):
-        # Get the current quiz field from the generator
-        question_num = player.current_question
-        return {
-            'question_num': question_num,
-            'title': f"Question {question_num}/{NUMBER_OF_QUESTIONS_INITIAL_TEST}",
-            'sample_dict': SAMPLES_INITIAL_TEST.iloc[question_num-1].to_dict()
-        }
-
-    @staticmethod
-    def before_next_page(player: Player, timeout_happened):
-        # Access the current quiz field dynamically
-        question_num = player.current_question
-        user_answer = getattr(player, 'Question'+str(question_num))
-        real_answer = SAMPLES_INITIAL_TEST['class'].iloc[question_num-1]
-        if (user_answer == "Approve" and real_answer == 1) or (user_answer == "Decline" and real_answer == 0):
-            player.participant.payoff += player.random_number()
-            player.correct_answers += 1
-        # Increase question number
-        player.current_question += 1
-    
 
 quiz_pages = [Question for _ in range(NUMBER_OF_QUESTIONS_INITIAL_TEST)]
 
